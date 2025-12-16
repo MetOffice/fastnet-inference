30a61e8056704a2795c1f5afd9b524ba76aba30a
Nathan Simpson <nsimpson@turing.ac.uk>
2025-12-16 14:50:51 +0000

    starting to write full inference loop

--- src/fastnet_inference/eval.py
+++ src/fastnet_inference/eval.py
@@ -1,28 +1,41 @@
 import torch
 import numpy
 import logging
-from jaxtyping import Float
-from anemoi.datasets.data import Dataset
-from fastnet_inference.data import load_anemoi_era5
+from torch.utils.data import DataLoader
+from dataclasses import dataclass
+from fastnet_inference.data import AnemoiERA5Dataset 
 from fastnet_inference.variables import FORECAST_VARS_ORDER_FASTNET 
 
 
 F_LEN = len(FORECAST_VARS_ORDER_FASTNET)
 
-def process_batch(batch: Float[numpy.ndarray, "b v t g"], ds: Dataset) -> Float[torch.Tensor, "b t g v"]:
-    """b = batch, v = variable, t = time, g = grid""" 
-    batch = torch.from_numpy(batch).permute(0, 2, 3, 1)
-    batch = ((batch - ds.statistics['mean']) / ds.statistics['stdev']).float() 
 
+class Config:
+    dataset_path: str
+    batch_size: int
+    num_workers: int
+    rollout_steps: int
+    start_time: str | None = None
+    end_time: str | None = None
 
-def run_model():
-    
+def full_loop():
+    config = Config()
+    ds = AnemoiERA5Dataset(
+        dataset_path=config.dataset_path,
+        start=config.start_time,
+        end=config.end_time,
+        rollout_steps=config.rollout_steps,
+    )
 
+    def unnormalize(batch):
+        return batch * ds.std + ds.mean
 
-    for batch in ds:
-        # b = batch, v = variable, t = time, g = grid
-        # change dims from bvtg to btgv (what FastNet expects)
-        batch = torch.from_numpy(batch).permute(0, 2, 3, 1)
-        batch = ((batch - ds.statistics['mean']) / ds.statistics['stdev']).float() 
+    dl = DataLoader(ds, batch_size=config.batch_size, num_workers=config.num_workers)
+
+    for batch in dl:
+        # FastNet expects forecast vars & forcings/constants as separate inputs
         forecast_features, non_forecast_features = batch[..., :F_LEN], batch[..., F_LEN:]
-        forecast_features = forecast_features.squeeze(1)
+        # separate out what we need for rollout
+        # grab initial conditions
+        init_forecast_features = forecast_features[:, 0, :, :]
+
