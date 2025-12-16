5ca3311b026a1c7cc39b3e3b3e9c7e9fbe4fe6bb
Nathan Simpson <nsimpson@turing.ac.uk>
2025-12-16 14:36:40 +0000

    add torch dataset

--- src/fastnet_inference/data.py
+++ src/fastnet_inference/data.py
@@ -1,23 +1,65 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset
 from anemoi.datasets import open_dataset
-from anemoi.datasets.data.dataset import Dataset
+from anemoi.datasets.data.dataset import Dataset as AnemoiDataset
 
 from fastnet_inference.variables import FORECAST_VARS_ORDER_FASTNET, NONFORECAST_VARS_ORDER_FASTNET, LONGHAND_VARIABLE_TO_ANEMOI_ERA5_SHORTHAND
 
 
-def load_anemoi_era5(dataset_name: str, select: list[str] | None = None) -> Dataset:
+def _load_anemoi_era5(dataset_path: str, start: str | None, end: str | None) -> AnemoiDataset:
     ordered_vars: list[str] = []
     for var, level in FORECAST_VARS_ORDER_FASTNET:
-        if select is not None and var not in select: continue
         short_name = LONGHAND_VARIABLE_TO_ANEMOI_ERA5_SHORTHAND[var]
         if level != 0:
             short_name += f"_{level}"
         ordered_vars.append(short_name)
 
     for var, level in NONFORECAST_VARS_ORDER_FASTNET:
-        if select is not None and var not in select: continue
         short_name = LONGHAND_VARIABLE_TO_ANEMOI_ERA5_SHORTHAND[var]
         if level != 0:
             short_name += f"_{level}"
         ordered_vars.append(short_name)
 
-    return open_dataset(dataset_name, select=ordered_vars)
+    return open_dataset(dataset_path, select=ordered_vars, start=start, end=end)
+
+
+class AnemoiERA5Dataset(Dataset):
+    def __init__(
+        self,
+        dataset_path: str,
+        start: str | None = None,
+        end: str | None = None,
+        rollout_steps: int = 0,
+    ) -> None:
+        # will load data with feature dim order of:
+        # forecast features, then nonforecast features
+        self.ds = _load_anemoi_era5(
+            dataset_path=dataset_path,
+            start=start,
+            end=end,
+        )
+        if len(np.unique(np.diff(self.ds.dates))) > 1:
+            msg = "Time periods in specified time range are not contiguous!"
+            raise ValueError(msg)
+        self.rollout_steps = rollout_steps
+        self.mean = self.ds.statistics['mean']
+        self.std = self.ds.statistics['stdev']
+        # calculate actual number of data points based on rollout window
+        ds_size = len(self.ds)
+        if ds_size <= self.rollout_steps:
+            msg = f"Dataset only has {ds_size} entries - impossible to roll out to {rollout_steps} lead times!"
+            raise ValueError(msg)
+        self.num_samples = len(self.ds) - self.rollout_steps
+
+    def __len__(self) -> int: return self.num_samples 
+
+    def __getitem__(self, idx: int) -> torch.Tensor:
+        batch = self.ds[idx:idx+self.rollout_steps + 1]
+        # t = time, v = variable, e = ensemble, g = grid
+        # change dims from tveg to tgv (what FastNet expects)
+        # (no ensemble, as deterministic model)
+        # we'll be throwing away all the forecast features besides the init condition!
+        batch = torch.from_numpy(batch).squeeze(2).permute(0, 2, 1)
+        # normalize by stats (anemoi-datasets yaml recipe controls window for stats calc)
+        return ((batch - self.mean) / self.std).float() 
--- src/fastnet_inference/eval.py
+++ src/fastnet_inference/eval.py
@@ -1,8 +1,28 @@
 import torch
+import numpy
 import logging
 from jaxtyping import Float
+from anemoi.datasets.data import Dataset
 from fastnet_inference.data import load_anemoi_era5
+from fastnet_inference.variables import FORECAST_VARS_ORDER_FASTNET 
 
 
-def process_batch(batch: Float[torch.Tensor, "b v t g"]) -> Float[torch.Tensor, "b t g v"]:
+F_LEN = len(FORECAST_VARS_ORDER_FASTNET)
 
+def process_batch(batch: Float[numpy.ndarray, "b v t g"], ds: Dataset) -> Float[torch.Tensor, "b t g v"]:
+    """b = batch, v = variable, t = time, g = grid""" 
+    batch = torch.from_numpy(batch).permute(0, 2, 3, 1)
+    batch = ((batch - ds.statistics['mean']) / ds.statistics['stdev']).float() 
+
+
+def run_model():
+    
+
+
+    for batch in ds:
+        # b = batch, v = variable, t = time, g = grid
+        # change dims from bvtg to btgv (what FastNet expects)
+        batch = torch.from_numpy(batch).permute(0, 2, 3, 1)
+        batch = ((batch - ds.statistics['mean']) / ds.statistics['stdev']).float() 
+        forecast_features, non_forecast_features = batch[..., :F_LEN], batch[..., F_LEN:]
+        forecast_features = forecast_features.squeeze(1)
