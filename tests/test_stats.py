8e79d9eba801bd94bcb3d88271ff3846c11262fd
Sophie Arana <arana.sophie@gmail.com>
2026-04-08 14:53:51 +0100

    fix lint: sort imports and formatting

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -5,10 +5,10 @@ import torch
 
 from fastnet_inference.data import (
     GRAVITATIONAL_ACCELERATION,
-    AnemoiERA5Dataset,
     UNSTANDARDIZED_FEATURES,
+    AnemoiERA5Dataset,
+    get_fastnet_var_order,
 )
-from fastnet_inference.data import get_fastnet_var_order
 
 
 def test_stats_var_order():
@@ -35,9 +35,9 @@ def test_orography_scaling_and_unstandardized_features(synthetic_era5):
     normalized, _ = ds[0]
 
     orography_idx = ordered.index("orography")
-    expected_orog = (raw[..., orography_idx] * GRAVITATIONAL_ACCELERATION - ds.mean[orography_idx]) / ds.std[
-        orography_idx
-    ]
+    expected_orog = (
+        raw[..., orography_idx] * GRAVITATIONAL_ACCELERATION - ds.mean[orography_idx]
+    ) / ds.std[orography_idx]
     assert torch.allclose(normalized[..., orography_idx], expected_orog.float())
 
     for var in UNSTANDARDIZED_FEATURES:
